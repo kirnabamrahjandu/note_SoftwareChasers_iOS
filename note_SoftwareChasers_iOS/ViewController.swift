8dd09b78e88985e51c71f0f9b200224151f8d2ca
kirna <kirna1851994@gmail.com>
2021-02-02 10:09:59 -0800

    Initial Commit

---
+++ note_SoftwareChasers_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  note_SoftwareChasers_iOS
+//
+//  Created by user191875 on 2/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
