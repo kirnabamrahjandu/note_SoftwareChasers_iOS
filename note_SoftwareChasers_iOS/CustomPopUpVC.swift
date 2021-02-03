d74ff7e09c9813b3f2e4179d131d0bdf79de0b45
Shiwani03 <shiwanimalhotra0330@gmail.com>
2021-02-03 05:08:29 -0500

    submit and handle buttons

--- note_SoftwareChasers_iOS/CustomPopUpVC.swift
+++ note_SoftwareChasers_iOS/CustomPopUpVC.swift
@@ -20,11 +20,62 @@ class CustomPopUpVC: UIViewController {
     @IBOutlet weak var mainViewY: NSLayoutConstraint!
     @IBOutlet weak var mainView: UIView!
     var arrItems = [String]()
+    var arrSubject = [Subjects]()
+    var seletedSubject = Subjects()
+    var selected = -1
+    var popupTitle = "Subjects"
+    var delegate : delegate?
+    var subjectDelegate : subjectDelegate?
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func handleSubmit(_ sender: Any) {
+        subjectDelegate?.selectedSubject(name: seletedSubject)
+        delegate?.seletedItem(selected: selected)
+        cancelPopUp()
+    }
     
+    @IBAction func handleCancel(_ sender: Any) {
+        cancelPopUp()
+    }
+    @objc func cancelPopUp(){
+        mainViewY.constant = self.view.frame.height
+        
+            self.view.layoutIfNeeded()
+        
+            self.dismiss(animated: false)
+        }
+        
+    }
+    
+extension CustomPopUpVC : UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrItems.count == 0 ? arrSubject.count : arrItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! CustomPopUpTVC
+        let label = arrItems.count == 0 ? arrSubject[indexPath.row].subject_name : arrItems[indexPath.row]
+        cell.lblSubjectName.text = label
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if arrItems.count == 0 {
+            seletedSubject = arrSubject[indexPath.row]
+        }
+        else{
+            selected = indexPath.row
+        }
+    }
+    
+    
+}
+class CustomPopUpTVC: UITableViewCell {
+    @IBOutlet weak var lblSubjectName : UILabel!
+}
+
+
 
    
 
-}
+
