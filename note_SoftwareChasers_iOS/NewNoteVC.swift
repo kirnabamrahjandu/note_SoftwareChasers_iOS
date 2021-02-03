e52fd150f526f1c8bcc206cfa30ae749cefdcd3a
Harpreet Kaur <harpreet1998kaur31@gmail.com>
2021-02-03 14:31:54 +0530

    handling the data

--- note_SoftwareChasers_iOS/NewNoteVC.swift
+++ note_SoftwareChasers_iOS/NewNoteVC.swift
@@ -10,14 +10,35 @@ import CoreData
 import CoreLocation
 
 class NewNoteVC: UIViewController {
+    @IBOutlet weak var btnInfo: UIButton!
+    
+    @IBOutlet weak var btnImageClose: UIButton!
+    @IBOutlet weak var imgHeight: NSLayoutConstraint!
+    @IBOutlet weak var lblSubjectName: UILabel!
+    @IBOutlet weak var txtTitle: UITextField!
+    @IBOutlet weak var txtDescription: UITextView!
+    @IBOutlet weak var imgAttachmentView: UIImageView!
+    
+    var note : Note?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    @IBAction func handleDelete(_ sender: Any) {
+        imgHeight.constant = 20
+        btnImageClose.isHidden = true
+        imgAttachmentView.image = nil
+    }
     
-
+    @IBAction func handleInfo(_ sender: Any) {
+        let vc = self.storyboard?.instantiateViewController(identifier: "MoreInfoTC") as! MoreInfoTC
+        vc.note = note!
+        vc.subject = lblSubjectName.text
+        self.present(vc, animated: true, completion: nil)
+    }
+}
     /*
     // MARK: - Navigation
 
@@ -28,4 +49,4 @@ class NewNoteVC: UIViewController {
     }
     */
 
-}
+
