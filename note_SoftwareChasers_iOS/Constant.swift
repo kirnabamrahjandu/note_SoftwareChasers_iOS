84171332e4b8d731c145465cb11fb6944938aef0
Sarabjeet <infosarabjeetkaursidhu@gmail.com>
2021-02-02 23:32:07 -0800

    constant

---
+++ note_SoftwareChasers_iOS/Constant.swift
@@ -0,0 +1,28 @@
+//
+//  Constant.swift
+//  note_SoftwareChasers_iOS
+//
+//  Created by user186818 on 2/2/21.
+//
+
+import Foundation
+
+struct Constants {
+    //Table's Identifier Cells
+    static let SimpleNoteCell = "SimpleNoteCell"
+    static let ImageNoteCell = "ImageNoteCell"
+    //Note Constants
+    static let note_id = "note_id"
+    static let note_title = "note_title"
+    static let note_desc = "note_desc"
+    static let date_created = "date_created"
+    static let date_modified = "date_modified"
+    static let lat = "lat"
+    static let long = "long"
+    static let picture = "picture"
+    static let subject_id = "subject_id"
+    static let subject_name = "subject_name"
+    //Notification Center
+    static let send_subject = "send_subject"
+    
+}
