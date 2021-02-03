a38bfad689d2078c3f23145f77d93dc6feca06b5
kirna <kirna1851994@gmail.com>
2021-02-02 23:59:05 -0800

    more info file add

---
+++ note_SoftwareChasers_iOS/MoreInfoTVC.swift
@@ -0,0 +1,26 @@
+//
+//  MoreInfoTVC.swift
+//  note_SoftwareChasers_iOS
+//
+//  Created by user191875 on 2/2/21.
+//
+
+import UIKit
+
+class MoreInfoTVC: UITableViewController {
+    @IBOutlet weak var lblTitle : UILabel!
+    @IBOutlet weak var lblSub : UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+}
+    
+
+    
